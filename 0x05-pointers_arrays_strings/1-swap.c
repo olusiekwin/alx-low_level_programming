efc6185f5ad5752e91fb2436fef5954f6859aa4c
olusiekwin <olusiekwingraham@gmail.com>
2023-02-28 12:17:47 +0300

    graham

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,16 @@
 #include "main.h"
-/***
- * swap_int - swaps the va;lue of an int a and b
- * olusiekwin
- * @a: first int
- * @b: second int
- * return 0
- */
-
+/**
+* swap_int - swaps the value of int a and int b
+* Owned by Bwave/Bright
+* @a: first int
+* @b: second int
+* Return 0
+*/
 void swap_int(int *a, int *b)
 {
-    int s;
+		int s;
 
-    s = *a;
-    *a = *b;
-    *b = s;
+		s = *a;
+		*a = *b;
+		*b = s;
 }
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,16 @@
 #include "main.h"
-/***
- * _strlen - string length always return the length of a string
- * olusiekwin
- * @s: char to be checked
- * Description:- this will always check for the length of a string
- * Retern: 0 if success
- * */
-
+/**
+ * _strlen - return the length of a string
+ * Owned By Bwave
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 is success
+ */
 int _strlen(char *s)
 {
-    int a = 0;
+	int a = 0;
 
-    for (; *s++;)
-            a++;
-    return (a);
+	for (; *s++;)
+		a++;
+	return (a);
 }
