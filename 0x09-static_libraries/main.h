1a1e36657c9ebc53226ce25af165de78875b8431
olusiekwin <olusiekwingraham@gmail.com>
2023-03-10 23:35:36 +0300

    0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,6 @@
-#ifndef MAIN.H
-#define MAIN.H
-#include <stddef.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+
+#endif /* MAIN_H */
