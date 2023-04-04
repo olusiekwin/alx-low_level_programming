f84acc035a247577d956ca2c054551d1fd43b3d3
olusiekwin <olusiekwingraham@gmail.com>
2023-04-04 22:38:59 +0300

    103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+* find_listint_loop - finds the start of a loop in a linked list
+* @head: pointer to the head of the linked list
+*
+* Return: the address of the node where the loop starts, or NULL if there is no loop
+*/
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+
+	if (head == NULL)
+		return (NULL);
+
+	tortoise = hare = head;
+	while (hare != NULL && hare->next != NULL)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (tortoise);
+		}
+	}
+
+	return (NULL);
+}
+
