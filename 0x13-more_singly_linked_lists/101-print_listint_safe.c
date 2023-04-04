0d06bc8a17dfb43824ec5710146e7ac7fd73c47f
Gramm <85841088+olusiekwin@users.noreply.github.com>
2023-04-05 00:25:38 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,61 +1,29 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
- * realloc_list - reallocates memory for an array of pointers
+ * _r - reallocates memory for an array of pointers
  * to the nodes in a linked list
  * @list: the old list to append
  * @size: size of the new list (always one more than the old list)
- * @new_node: new node to add to the list
+ * @new: new node to add to the list
  *
  * Return: pointer to the new list
  */
-const listint_t **realloc_list(const listint_t **list, size_t size, const listint_t *new_node)
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 {
-	const listint_t **new_list;
+	const listint_t **newlist;
 	size_t i;
 
-	new_list = malloc(size * sizeof(listint_t *));
-	if (new_list == NULL)
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
 	{
 		free(list);
 		exit(98);
 	}
 	for (i = 0; i < size - 1; i++)
-		new_list[i] = list[i];
-	new_list[i] = new_node;
+		newlist[i] = list[i];
+	newlist[i] = new;
 	free(list);
-	return (new_list);
-}
-
-/**
- * print_listint_safe - prints a listint_t linked list.
- * @head: pointer to the start of the list
- *
- * Return: the number of nodes in the list
- */
-size_t print_listint_safe(const listint_t *head)
-{
-	size_t i, num = 0;
-	const listint_t **list = NULL;
-
-	while (head != NULL)
-	{
-		for (i = 0; i < num; i++)
-		{
-			if (head == list[i])
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free(list);
-				return (num);
-			}
-		}
-		num++;
-		list = realloc_list(list, num, head);
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-	}
-	free(list);
-	return (num);
+	return (newlist);
 }
