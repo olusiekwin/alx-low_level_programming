b30bda6295a7d247092dcb4d1e704a2ff7c5eab1
Gramm <85841088+olusiekwin@users.noreply.github.com>
2023-04-05 00:38:50 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,15 +1,16 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
- * _r - reallocates memory for an array of pointers
- * to the nodes in a linked list
- * @list: the old list to append
- * @size: size of the new list (always one more than the old list)
- * @new: new node to add to the list
- *
- * Return: pointer to the new list
- */
+
+* _realloc_list - reallocates memory for an array of pointers
+*	to the nodes in a linked list
+* @list: the old list to append
+* @size: size of the new list (always one more than the old list)
+* @new_node: new node to add to the list
+* Return: pointer to the new list
+*/
 const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 {
 	const listint_t **newlist;
