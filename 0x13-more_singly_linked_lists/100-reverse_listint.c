2c4a20b8234ba9b5302f8298e7b9140832ca1859
Gramm <85841088+olusiekwin@users.noreply.github.com>
2023-04-05 00:28:08 +0300

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,31 +1,22 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * print_listint_safe - prints a listint_t linked list.
- * @head: pointer to the start of the list
+ * reverse_listint - reverses a listint_t linked list
+ * @head: pointer to pointer to the head node of the list
  *
- * Return: the number of nodes in the list
+ * Return: pointer to the first node of the reversed list
  */
-size_t print_listint_safe(const listint_t *head)
+listint_t *reverse_listint(listint_t **head)
 {
-	size_t count = 0;
-	const listint_t *temp = head, *loop_start = NULL;
+	listint_t *prev = NULL, *next;
 
-	while (temp)
+	while (*head != NULL)
 	{
-		count++;
-		printf("[%p] %d\n", (void *)temp, temp->n);
-		temp = temp->next;
-		if (temp >= loop_start)
-		{
-			printf("-> [%p] %d\n", (void *)temp, temp->n);
-			exit(98);
-		}
-		if (!loop_start && temp)
-			loop_start = temp;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 
-	return (count);
+	return prev;
 }
