5322d82a4aff28ce54a38c231db46c15d7794321
olusiekwin <olusiekwingraham@gmail.com>
2023-04-04 14:57:27 +0300

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,12 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define SUCCESS 1
-#define FAIL -1
 /**
  * struct listint_s - singly linked list
  * @n: integer
