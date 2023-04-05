ef83a83e57642232aec5840da7acaca8ff11467f
olusiekwin <olusiekwingraham@gmail.com>
2023-04-05 09:18:34 +0300

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,34 +1,65 @@
 #include "lists.h"
+#include <stdlib.h>
 #include <stdio.h>
+
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
+ * _ra - reallocates memory for an array  pointer
+ * to the nodes contained in a linked list.
+ * @list: The old list being appended.
+ * @size: Size of the new list always one more than the old list.
+ * @new: New node  added to the list
  *
- * Return: number of elements in the freed list
+ * Return: Pointer to the new list
  */
-size_t free_listint_safe(listint_t **h)
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
-size_t len = 0;
-int diff;
-listint_t *temp;
-if (!h || !*h)
-return (0);
-while (*h)
-{
-diff = *h - (*h)->next;
-if (diff > 0)
-{
-temp = (*h)->next;
-*h = temp;
-len++;
+	listint_t **newSequence;
+	size_t i;
+
+	newSequence = malloc(size * sizeof(listint_t *));
+	if (newSequence == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newSequence[i] = list[i];
+	newSequence[i] = new;
+	free(list);
+	return (newSequence);
 }
-else
+
+/**
+ * free_listint_safe - Frees  listint_t linked list.
+ * @head: Double pointer to the start of the list.
+ *
+ * Return: Number of nodes present in the list.
+ */
+size_t free_listint_safe(listint_t **head)
 {
-*h = NULL;
-len++;
-break;
-}
-}
-*h = NULL;
-return (len);
+	size_t i, my_num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
+
+	if (head == NULL || *head == NULL)
+		return (my_num);
+	while (*head != NULL)
+	{
+		for (i = 0; i < my_num; i++)
+		{
+			if (*head == list[i])
+			{
+				*head = NULL;
+				free(list);
+				return (my_num);
+			}
+		}
+		my_num++;
+		list = _ra(list, my_num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (my_num);
 }
