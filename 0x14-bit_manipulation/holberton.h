e740c42e57409fb6feab5c06e7e1566ed11c58a0
olusiekwin <olusiekwingraham@gmail.com>
2023-04-06 23:03:30 +0300

    holberton.h

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif /* HOLBERTON_H */
