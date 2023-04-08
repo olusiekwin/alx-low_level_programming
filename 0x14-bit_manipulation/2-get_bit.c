c8ec3be7b943722b6d88ed71f4ec7bfecbdcff55
olusiekwin <olusiekwingraham@gmail.com>
2023-04-08 07:40:00 +0300

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * get_bit_value - Gets the value of the bit at a given index.
+ * @digit: The number to search the bit in.
+ * @index: The index of the bit.
+ *
+ * Return: The value of the bit at the given index or -1 if an error occurred.
+ */
+int get_bit_value(unsigned long int digit, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	return ((digit >> index) & 1);
+}
+
