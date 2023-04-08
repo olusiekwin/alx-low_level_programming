84478cf472f9ba1883714e81823ea7488765185f
olusiekwin <olusiekwingraham@gmail.com>
2023-04-09 02:04:34 +0300

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_N
+#ifndef HEADER_H
+#define HEADER_H
 
 #include <elf.h>
 #include <stdlib.h>
@@ -14,16 +14,18 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 /**
- * Header functions for ELF
+ * Functions related to ELF header
 */
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
-#endif
+void verify_elf(unsigned char *e_ident);
+void print_elf_magic(unsigned char *e_ident);
+void print_elf_class(unsigned char *e_ident);
+void print_elf_data(unsigned char *e_ident);
+void print_elf_version(unsigned char *e_ident);
+void print_elf_abi(unsigned char *e_ident);
+void print_elf_osabi(unsigned char *e_ident);
+void print_elf_type(unsigned int e_type, unsigned char *e_ident);
+void print_elf_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf_file(int elf);
+
+#endif /* HEADER_H */
+
