eaa5a16e92330d5c3ef8aa8aeb79c5942a2b470f
olusiekwin <olusiekwingraham@gmail.com>
2023-04-08 06:32:56 +0300

    main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+#endif /*MAIN_H */
