3ce3693038502292d5721050e33d15688232665a
olusiekwin <olusiekwingraham@gmail.com>
2023-04-10 18:52:39 +0300

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,33 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <elf.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include <unistd.h>
 
-
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 
-/**
- * Functions related to ELF header
-*/
-void verify_elf(unsigned char *e_ident);
-void print_elf_magic(unsigned char *e_ident);
-void print_elf_class(unsigned char *e_ident);
-void print_elf_data(unsigned char *e_ident);
-void print_elf_version(unsigned char *e_ident);
-void print_elf_abi(unsigned char *e_ident);
-void print_elf_osabi(unsigned char *e_ident);
-void print_elf_type(unsigned int e_type, unsigned char *e_ident);
-void print_elf_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf_file(int elf);
-
-#endif /* HEADER_H */
-
+#endif /*MAIN_H*/
